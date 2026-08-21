31fb1c3c5d37e75fe40c7f802b5282c977c6e8ce
Pieter Van Mullem <pietervanmullem@gmail.com>
2026-08-21 21:24:19 +0200

    Fix Odoo domain syntax in fetch_personeelskosten

--- src/odoo_client.py
+++ src/odoo_client.py
@@ -107,10 +107,12 @@ def fetch_personeelskosten() -> list:
     uid = get_uid()
     models = get_models()
 
+    lonen = "te betalen lonen"
+    bezoldigingen = "te betalen bezoldigingen"
     accounts = models.execute_kw(
         ODOO_DB, uid, ODOO_PASSWORD,
         "account.account", "search_read",
-        [[["name", "ilike", "te betalen lonen"], "|", ["name", "ilike", "te betalen bezoldigingen"]]],
+        [["|", ["name", "ilike", lonen], ["name", "ilike", bezoldigingen]]],
         {"fields": ["id", "name", "code"]}
     )
     if not accounts:
